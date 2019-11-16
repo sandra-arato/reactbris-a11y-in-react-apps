5148a6fcb7af9188d50ec6aaad4ebc0ea87a5df2
Sandra Arato <sandra@idoescode.com>
2019-11-16 16:40:37 +1000

    end of step1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+if (process.env.NODE_ENV !== 'production') {
+    var axe = require('react-axe');
+    axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
